5a6b8d417be4a31961929d4db3c3b474dfdfb042
NurNajwa-1509 <97671680+NurNajwa-1509@users.noreply.github.com>
2023-10-26 18:33:13 +0800

    Add files via upload

---
+++ 100/Atm.py
@@ -0,0 +1,15 @@
+class Atm:
+    def __init__(self,cardnumber,pin):
+        self.cardnumber=cardnumber
+        self.pin=pin
+
+    def check_balance(self):
+        print('Your balance is 50000')
+
+        new_user = Atm(2564678,5484)
+
+    
+
+   
+    
+        
